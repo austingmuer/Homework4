37bc50ce57432fa914029e0d879284090e79cb84
Austin Gmuer <a.gmuer@setonhill.edu>
2020-02-25 12:21:32 -0500

    Initial Commit

---
+++ Homework4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework4
+//
+//  Created by Austin Gmuer on 2/25/20.
+//  Copyright © 2020 Austin Gmuer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
